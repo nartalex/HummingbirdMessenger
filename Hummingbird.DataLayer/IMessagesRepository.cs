bef231a81e97bfe89507fdefa3dd5fb3fa775acf
Nartalex <nartalex@ya.ru>
2017-10-14 23:23:59 +0300

    Поправил интерфейс сообщений

--- Hummingbird.DataLayer/IMessagesRepository.cs
+++ Hummingbird.DataLayer/IMessagesRepository.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hummingbird.Model;
 
 namespace Hummingbird.DataLayer
 {
     interface IMessagesRepository
     {
-        
+        Message GetLastMessage(Guid userId, Guid chatId);
+        IEnumerable<Message> GetAmountOfMessages(Guid chatId, int amount);
+        void DeleteMessage(Guid id);
+        void EditMessage(Guid id);
     }
 }
